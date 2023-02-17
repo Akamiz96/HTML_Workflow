acb3e61fc7c9e15b4c941b6f9860b47bf63da6f7
Alejo Castro <alejandrocastro@javeriana.edu.co>
2023-02-17 18:53:11 -0500

    Fix app.js to get deploy in Render

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3001;
 
-app.listen(3000, () => {
-  console.log("Application started and Listening on port 3000");
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 // serve your css as static
 app.use(express.static(__dirname));
